eb69adce972452c257416a6b19ceb1d06646d6f3
Rajeev-Foryou <mandalrajeev3@gmail.com>
2025-03-05 11:26:37 +0530

    Milestone-16

--- Frontend/src/Routess.js
+++ Frontend/src/Routess.js
@@ -4,6 +4,8 @@ import ActivationPage from "../src/pages/ActivationPage.jsx";
 import Home from "../src/pages/Home.jsx";
 import CreateProduct from "../src/pages/createProduct.jsx";
 import MyProducts from "../src/pages/myProducts.jsx";
+import Cart from "./pages/cart.jsx";
+import ProductDetails from "./pages/productDetails.jsx";
 export {
   LoginPage,
   SignUpPage,
@@ -11,4 +13,6 @@ export {
   Home,
   CreateProduct,
   MyProducts,
+  Cart,
+  ProductDetails,
 };
